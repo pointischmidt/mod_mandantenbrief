8d335d5ee3f771fcb90c7bd178696564c6419f95
pointischmidt <pointsquare@googlemail.com>
2025-11-03 20:02:37 +0100

    Fix mod_mandantenbrief.php - Use simple stable version (247 chars)

--- mod_mandantenbrief.php
+++ mod_mandantenbrief.php
@@ -1,34 +1,13 @@
 <?php
 /**
- * @package     Joomla.Site
- * @subpackage  mod_mandantenbrief
- *
+ * @package     mod_mandantenbrief
  * @copyright   Copyright (C) 2025 djumla.dev
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @license     GNU General Public License version 2 or later
  */
 
 defined('_JEXEC') or die;
 
-// Ensure class file is loaded (bypass autoloader issues)
-require_once __DIR__ . '/src/Helper/Module.php';
-
-use Joomla\CMS\Helper\ModuleHelper;
 use ModMandantenbrief\Site\Helper\Module;
 
-// Initialize module with proper error handling
-try {
-    $moduleInstance = new Module($module, $params);
-    $articles = $moduleInstance->getArticles();
-    $moduleClasses = $moduleInstance->getModuleClasses();
-    $debugInfo = $moduleInstance->getDebugInfo();
-    
-    // Load template
-    require ModuleHelper::getLayoutPath('mod_mandantenbrief', $params->get('layout', 'default'));
-    
-} catch (Exception $e) {
-    // Fallback for development
-    echo '<div class="uk-alert uk-alert-danger">';
-    echo '<h4>Modul-Fehler</h4>';
-    echo '<p>' . htmlspecialchars($e->getMessage()) . '</p>';
-    echo '</div>';
-}
+$helper = new Module($module, $params);
+require JModuleHelper::getLayoutPath('mod_mandantenbrief', $params->get('layout', 'default'));
